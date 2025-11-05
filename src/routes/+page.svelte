c22551ed00664c80d55d0b20a64c97ecf0c26f3a
gabors0 <gabors0@proton.me>
2025-11-05 21:45:48 +0100

    shortcuts unfinished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Searchbar from "../lib/Searchbar.svelte";
+    import Shortcut from "../lib/Shortcut.svelte";
 </script>
 
 <div class="flex flex-col justify-center items-center">
@@ -11,6 +12,24 @@ _/    _/  _/    _/  _/    _/  _/    _/  _/        _/  _/
  _/_/_/  _/    _/    _/_/_/    _/_/_/    _/_/_/  _/    _/
 </pre>
     <Searchbar />
+    <div class="flex max-w-[27rem] gap-4 flex-row flex-wrap">
+        <Shortcut
+            icon="https://media.discordapp.net/attachments/1304948416979140661/1434994989057835209/image.png?ex=690c54fa&is=690b037a&hm=d3ed943719f99122e7f3dee5955e03a7a7afecbc218b7bd55a6b1e965aff8b07&=&format=webp&quality=lossless"
+            title="test"
+            url="https://gs0.me"
+        />
+        <Shortcut
+            icon="https://media.BADdiscordapp.net/attachments/1304948416979140661/1434994989057835209/image.png?ex=690c54fa&is=690b037a&hm=d3ed943719f99122e7f3dee5955e03a7a7afecbc218b7bd55a6b1e965aff8b07&=&format=webp&quality=lossless"
+            title="test"
+            url="https://gs0.me"
+        />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+        <Shortcut icon="" title="test" url="https://gs0.me" />
+    </div>
 </div>
 
 <footer class="absolute bottom-0 left-0 p-5">
